16c4dfcedb706fce235d9fa015a195dd7397a25e
bitprinter <bitprinter@fastmail.fm>
2013-12-01 20:51:12 -0500

    Add verbose logging to disk.sh

--- src/script/disk.sh
+++ src/script/disk.sh
@@ -36,12 +36,15 @@ EOF
 set -e
 
 # Give fdisk a couple seconds to finish
+echo "fdisk complete - Waiting 5 seconds ..."
 sleep 5
 
 # Run setup on the formatted loop device
+echo "Running losetup ..."
 losetup -d $DEVICE
 
 # Add mappings for our new device and store the output for mounting
+echo "Running kpartx ..."
 PARTS=`kpartx -va $IMAGE | cut -d ' ' -f 3`
 PART_BOOT=`echo "$PARTS" | head -n 1`
 PART_ROOT=`echo "$PARTS" | tail -n 1`
@@ -50,14 +53,20 @@ PART_ROOT=`echo "$PARTS" | tail -n 1`
 MAPPER=/dev/mapper
 BOOTP=$MAPPER/$PART_BOOT
 ROOTP=$MAPPER/$PART_ROOT
+echo "Boot partition mapped to: $BOOTP"
+echo "Root partition mapped to: $ROOTP"
 
 # Format partitions
+echo "Formatting partitions ..."
 mkfs.vfat $BOOTP
 mkfs.ext4 $ROOTP
 
 # Make temporary mnt directory and mount image
+echo "Mounting partitions ..."
 mkdir -p $MOUNT_DIR
 mount $ROOTP $MOUNT_DIR
 
 mkdir -p $MOUNT_DIR/boot
 mount $BOOTP $MOUNT_DIR/boot
+
+echo "Done!"
